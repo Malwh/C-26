ffd856236877cd31551f6bae12c57d7a35661319
Malwh <arunsardeshmukh@gmail.com>
2021-03-23 19:31:29 +0530

    add p1

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,p1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    p1 = new Ground(150, 305, 350, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -49,6 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+    
 
     box3.display();
     box4.display();
@@ -60,5 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    p1.display();
 }
